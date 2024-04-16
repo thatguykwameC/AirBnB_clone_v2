613d56f235b655d0d80b8b29b1dea627ebefbce0
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-16 07:34:17 -0700

    Updated place

--- models/place.py
+++ models/place.py
@@ -5,6 +5,14 @@ from sqlalchemy import Column, String, Float, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
+merge_table = Table("place_amenity", Base.metadata,
+                    Column("place_id", String(60), ForeignKey('places.id'),
+                           primary_key=True, nullable=False),
+                    Column("amenity_id", String(60),
+                           ForeignKey('amenities.id'),
+                           primary_key=True, nullable=False))
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -20,3 +28,25 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
+
+    @property
+    def reviews(self):
+        """ list of linked reviews """
+        from models import storage
+        return [r for r in storage.all(Review).values()
+                if r.place_id == self.id]
+
+    @property
+    def amenities(self):
+        """ list of all linked amenities """
+        from models import storage
+        return [a for a in storage.all(Amenity).values()
+                if a.id in self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, param):
+        """ linked amenities """
+        if isinstance(param, Amenity):
+            self.amenity_ids.append(param.id)
