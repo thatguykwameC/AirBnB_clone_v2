b6943808c4101c94653949e7892b1bc72737fe9e
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-16 06:33:45 -0700

    Refactor

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = self.__dict__.copy()
-        dictionary['__class__'] = str(type(self).__name__)
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop("_sa_instance_state", None)
