6ed895854dca1134352f13a39f7403f2f2904958
thatguykwameC <thatguykwamec@gmail.com>
2024-05-20 23:10:06 +0000

    Starts a web app

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
 
-"""This module contains routes for the AirBnB web project."""
+"""This module starts a Flask web application."""
 
 from markupsafe import escape
 from flask import render_template
 from flask import Flask
 
-app = Flask(__name__)
+web_app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@web_app.route("/", strict_slashes=False)
 def home():
-    """Returns a simple string for the homepage."""
+    """Returns a string to the homepage."""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@web_app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Handles the /hbnb route."""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@web_app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
-    """Returns a note about the C language."""
+    """Returns C followed by a text"""
     return f"C {escape(text.replace('_', ' '))}"
 
 
-@app.route("/python/", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@web_app.route("/python/", strict_slashes=False)
+@web_app.route("/python/<text>", strict_slashes=False)
 def python_route(text="is cool"):
-    """Returns a note about the Python language."""
+    """Returns Python followed by a text."""
     return f"Python {escape(text.replace('_', ' '))}"
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@web_app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Returns a string stating an input is a number if it is an integer."""
+    """Returns a number if it is an integer."""
     return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@web_app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Renders information about a number."""
     return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    web_app.run(host="0.0.0.0", port=5000)
