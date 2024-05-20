cadd046fe4668114f395f24ffa777a37748007a6
thatguykwameC <thatguykwamec@gmail.com>
2024-05-20 22:34:40 +0000

    Starts a web app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module starts a Flask web application."""
+
+from flask import Flask
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def home():
+    """Returns a string to thehomepage."""
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Handles the /hbnb route."""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    web_app.run(host="0.0.0.0", port=5000)
